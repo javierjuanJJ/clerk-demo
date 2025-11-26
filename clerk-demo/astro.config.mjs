7790ec5ef236a4c7d9015cee635f01ad93d83f18
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-11-26 08:11:25 +0100

    Commit final

--- clerk-demo/astro.config.mjs
+++ clerk-demo/astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'
-import clerk from '@clerk/astro'
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { dark } from '@clerk/themes';
+import { esES } from '@clerk/localizations';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [clerk({
+    localization: esES,
+    appearance: {
+      baseTheme: dark
+    }
+  })],
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [clerk()],
-  adapter: node({ mode: 'standalone' }),
-  output: 'server',
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });
